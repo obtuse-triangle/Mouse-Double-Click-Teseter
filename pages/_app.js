0f50c548433ca840ffd702629283777671397213
obtuse@obtuse.kro.kr <obtuse@obtuse.kro.kr>
2022-08-17 16:54:40 +0900

    네이버 웹마스터 등록용 meta태그

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,10 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta
+          name="naver-site-verification"
+          content="1e2ef7dcd0a869df3f7f0fd29d807c93aba83881"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
